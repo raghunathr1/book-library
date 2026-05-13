62c92282872ec964e629caefe71533589a525207
raghunathr1 <raghunthkapase2004@gmail.com>
2026-05-13 16:12:24 +0530

    Fixing Style of Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,15 +5,12 @@ function Header() {
 
   return (
     <nav className="nav">
-
       <h1>Book Library</h1>
-
       <ul id="head2">  
           <Link to="/" className="h3">Home</Link>
           <Link to="/browse" className="h3">Browse</Link>
           <Link to="/add" className="h3">Add</Link>
       </ul>
-
     </nav>
   )
 }
